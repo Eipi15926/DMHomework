7b91ebfdf9ba48c53b1219c3f80954d55bf8cb7e
RollingYoyoball <5958628@qq.com>
2023-10-19 23:39:39 +0800

    updated detr-main

--- detr-main/engine.py
+++ detr-main/engine.py
@@ -60,7 +60,8 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
-    print("Averaged stats:", metric_logger)
+    #print("Averaged stats:", metric_logger)
+    print("Averaged stats calculated.")
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
 
--- detr-main/util/misc.py
+++ detr-main/util/misc.py
@@ -67,7 +67,10 @@ class SmoothedValue(object):
 
     @property
     def global_avg(self):
-        return self.total / self.count
+        if self.count>0:
+            return self.total / self.count
+        else:
+            return self.total
 
     @property
     def max(self):
@@ -242,8 +245,8 @@ class MetricLogger(object):
             end = time.time()
         total_time = time.time() - start_time
         total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-        print('{} Total time: {} ({:.4f} s / it)'.format(
-            header, total_time_str, total_time / len(iterable)))
+        #print('{} Total time: {} ({:.4f} s / it)'.format(
+        #    header, total_time_str, total_time / len(iterable)))
 
 
 def get_sha():
